deaf8ebf658435138b1388b5f87011b08cbfc9fd
Ryan Gregory <ryansynez@gmail.com>
2023-11-19 20:27:27 -0600

    completed GA5

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // Write your JavaScript code here!
 
+
 window.addEventListener("load", function() {
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    
+
+    let list = document.getElementById("faultyItems");
+    let form = document.querySelector("form[data-testid=testForm]");
+    form.addEventListener("submit", function(event) {
+        //let list = document.getElementById("faultyItems").value;
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoMass = document.querySelector("input[name=cargoMass]").value;
+
+        event.preventDefault();
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+    });
+
+    let listedPlanets, planet;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
+        planet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
+       
+    });
     
  });
